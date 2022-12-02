b67a12c0a823e71a3e5137a750ec6b244e6b26eb
Christian Piet <43993129+Manbearpiet@users.noreply.github.com>
2022-12-02 11:07:11 +0100

    Day 1 prettier

--- 2022/day-1/pwsh/aoc-1-1.ps1
+++ 2022/day-1/pwsh/aoc-1-1.ps1
@@ -1,13 +1,16 @@
-$data = get-content ./input.txt
+# Not mine, from talarion on Discord
+
+$data = Get-Content ./input.txt
 
 $santa = @{}
 $i = 0
 
 foreach ($d in $data) {
-    if (!$d) {$i++}
-    $santa[$i]+=[int]$d
+    if (!$d) { $i++ }
+    $santa[$i] += [int]$d
 }
 
 [pscustomobject]@{
-part1 = $santa.values|Sort-Object -Descending|select -first 1
-part2 = $santa.values|Sort-Object -Descending|select -first 3 | Measure-Object -sum | % sum}
+    part1 = $santa.values | Sort-Object -Descending | select -First 1
+    part2 = $santa.values | Sort-Object -Descending | select -First 3 | Measure-Object -Sum | % sum
+}
